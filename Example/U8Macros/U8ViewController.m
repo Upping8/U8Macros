4691ff9f784e81b540f913de8970da33c8d25cf9
Upping8 <2298359591@qq.com>
2022-10-29 22:40:22 +0800

    add Macros and MeatMacros

--- Example/U8Macros/U8ViewController.m
+++ Example/U8Macros/U8ViewController.m
@@ -7,6 +7,28 @@
 //
 
 #import "U8ViewController.h"
+#import <U8Macros/U8Macros.h>
+
+@interface U8Person : NSObject
+@property (nonatomic, copy)void(^block)();
+@end
+@implementation U8Person
+
+- (instancetype)init {
+    if (self = [super init]) {
+        @weakify(self);
+        self.block = ^{
+//            @strongify(self);
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                [NSThread sleepForTimeInterval:2.0];
+                NSLog(@"strongify - %@", self_weak_); // null; without @strongify(self)
+            });
+        };
+    }
+    return self;
+}
+
+@end
 
 @interface U8ViewController ()
 
@@ -18,12 +40,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self testMacros];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)testMacros {
+    @onExit {
+        NSLog(@"onExit test");
+    };
+    NSLog(@"testMacros");
+    
+    //@strongify, @weakify
+    U8Person *person = [[U8Person alloc] init];
+    person.block();
 }
 
 @end
